7ec85664f8f0d35c6d174c2c5365f45df780e04c
xittz <a.patseev@gmail.com>
2018-09-16 16:48:12 +0300

    Лабораторная №2, задание №2. Пацеев Андрей

---
+++ lab2/task2/task-2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace task_2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 29);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(255, 249);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(280, 29);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(255, 249);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(280, 299);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(255, 249);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 299);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(28, 349);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 28);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Red";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(28, 377);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(81, 28);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Green";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(28, 406);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(66, 28);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Blue";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 299);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Выделить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 569);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Гистограммы компонент";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab2/task2/task-2/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_2
+{
+	public partial class Form1 : Form
+	{
+		private static ColorMatrix redMatrix = new ColorMatrix(new float[][] {
+				new float[] { 1, 1, 1, 0, 0 },
+				new float[] { 0, 0, 0, 0, 0 },
+				new float[] { 0, 0, 0, 0, 0 },
+				new float[] { 0, 0, 0, 1, 0 },
+				new float[] { 0, 0, 0, 0, 1 }
+			});
+   
+		private static ColorMatrix greenMatrix = new ColorMatrix(new float[][] {
+				new float[] { 0, 0, 0, 0, 0 },
+				new float[] { 1, 1, 1, 0, 0 },
+				new float[] { 0, 0, 0, 0, 0 },
+				new float[] { 0, 0, 0, 1, 0 },
+				new float[] { 0, 0, 0, 0, 1 }
+			});
+   
+		private static ColorMatrix blueMatrix = new ColorMatrix(new float[][] {
+				new float[] { 0, 0, 0, 0, 0 },
+				new float[] { 0, 0, 0, 0, 0 },
+				new float[] { 1, 1, 1, 0, 0 },
+				new float[] { 0, 0, 0, 1, 0 },
+				new float[] { 0, 0, 0, 0, 1 }
+			});
+
+        private Bitmap bitmap;
+
+        public Form1()
+		{
+			InitializeComponent();
+		}
+   
+		private static Bitmap ConvertColor(Bitmap source, ColorMatrix transformation)
+		{
+			Bitmap converted = new Bitmap(source.Width, source.Height);
+			ImageAttributes attributes = new ImageAttributes();
+			attributes.SetColorMatrix(transformation);
+			Graphics graphics = Graphics.FromImage(converted);
+			graphics.DrawImage(source, new Rectangle(0, 0, source.Width, source.Height),
+				0, 0, source.Width, source.Height,
+				GraphicsUnit.Pixel, attributes);
+			graphics.Dispose();
+			return converted;
+		}
+   
+		private static Bitmap Histogram(Bitmap image)
+		{
+			int[] colorDistribution = new int[256];
+			Bitmap histogram = new Bitmap(256, 256);
+			Graphics graphics = Graphics.FromImage(histogram);
+			int max = int.MinValue;
+			for (int row = 0; row < image.Height; ++row)
+				for (int col = 0; col < image.Width; ++col)
+				{
+					Color c = image.GetPixel(col, row);
+					++colorDistribution[c.R];
+					if (colorDistribution[c.R] > max)
+                        max = colorDistribution[c.R];
+				}
+
+			for (int i = 0; i < 255; ++i)
+			{
+                int distrCur = 255 - (int)(256 * colorDistribution[i] / (double)max);
+                int distrNext = 255 - (int)(256 * colorDistribution[i + 1] / (double)max);
+				graphics.DrawLine(Pens.Black, i, distrCur, i + 1, distrNext);
+			}
+			graphics.Dispose();
+			return histogram;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var dialog = new OpenFileDialog();
+			if (DialogResult.OK != dialog.ShowDialog()) return;
+            try
+            {
+                bitmap = new Bitmap(dialog.FileName);
+            }
+            catch
+            {
+                MessageBox.Show("Cannot open file");
+                return;
+            }
+
+            pictureBox1.Image = bitmap;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                var red = ConvertColor(bitmap, redMatrix);
+                pictureBox2.Image = red;
+                pictureBox3.Image = Histogram(red);
+            }
+            else if (radioButton2.Checked)
+            {
+                var green = ConvertColor(bitmap, greenMatrix);
+                pictureBox2.Image = green;
+                pictureBox3.Image = Histogram(green);
+            }
+            else if (radioButton3.Checked)
+            {
+                var blue = ConvertColor(bitmap, blueMatrix);
+                pictureBox2.Image = blue;
+                pictureBox3.Image = Histogram(blue);
+            }
+        }
+    }
+}
