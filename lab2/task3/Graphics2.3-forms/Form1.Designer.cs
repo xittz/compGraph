f35e4434ac8a05edae8306ffc320cf8b3f281aaf
Oleg Smirnov <itech7@ya.ru>
2018-09-24 17:21:33 +0300

    Create task3

---
+++ lab2/task3/Graphics2.3-forms/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Graphics2._3_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(162, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Choose file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(10, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 49);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File not selected";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(34, 145);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(105, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "H";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "S";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "V";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(34, 172);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(105, 22);
+            this.numericUpDown2.TabIndex = 8;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(33, 201);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(105, 22);
+            this.numericUpDown3.TabIndex = 9;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(607, 284);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ lab2/task3/Graphics2.3-forms/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics2._3_forms
+{
+    public partial class Form1 : Form
+    {
+        Bitmap sourceBitmap;
+        Bitmap bitmap;
+
+        double H = 0.0, S = 0.0, V = 0.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static void ToHSV(Color color, out double hue, out double saturation, out double value)
+        {
+            int red = color.R;
+            int green = color.G;
+            int blue = color.B;
+
+            int maxRGB = Math.Max(red, Math.Max(green, blue));
+            int minRGB = Math.Min(red, Math.Min(green, blue));
+
+            int diff = maxRGB - minRGB;
+
+            // calc hue
+            if (diff == 0)
+                hue = 0;
+            else if (maxRGB == red)
+            {
+                if (green >= blue)
+                    hue = (60 * (green - blue)) / diff;
+                else
+                    hue = ((60 * (green - blue)) / diff) + 360;
+            }
+            else
+            {
+                if (maxRGB == green)
+                    hue = ((60 * (blue - red)) / diff) + 120;
+                else
+                    hue = ((60 * (red - green)) / diff) + 240;
+            }
+            //
+
+            saturation = (maxRGB == 0) ? 0 : 1d - (1d * minRGB / maxRGB);
+
+            value = maxRGB / 255d;
+
+        }
+
+
+        public static Color FromHSV(double hue, double saturation, double value)
+        {
+
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int vMin = Convert.ToInt32(value * Math.Max(0, (1 - saturation)));
+            int a = Convert.ToInt32((v - vMin) * ((hue % 60) / 60));
+            int vDec = v - a;
+            int vInc = vMin + a;
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, vInc, vMin);
+            else if (hi == 1)
+                return Color.FromArgb(255, vDec, v, vMin);
+            else if (hi == 2)
+                return Color.FromArgb(255, vMin, v, vInc);
+            else if (hi == 3)
+                return Color.FromArgb(255, vMin, vDec, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, vInc, vMin, v);
+            else
+                return Color.FromArgb(255, v, vMin, vDec);
+        }
+
+        private void updatePictureBox()
+        {
+            if (bitmap == null) return;
+
+            for (int x = 0; x < sourceBitmap.Width; ++x)
+                for (int y = 0; y < sourceBitmap.Height; ++y)
+                {
+                    Color color = sourceBitmap.GetPixel(x, y);
+                    double h, s, v;
+                    ToHSV(color, out h, out s, out v);
+
+                    double newS = Math.Max(0, Math.Min(1, s + this.S / 100));
+
+                    double newV = Math.Max(0, Math.Min(1, v + this.V / 100));
+
+                    Color newColor = FromHSV(h + H, newS, newV);
+
+                    bitmap.SetPixel(x, y, newColor);
+                }
+
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+            label1.Text = "Selected " + openFileDialog1.SafeFileName;
+
+            bitmap = new Bitmap(openFileDialog1.FileName);
+            sourceBitmap = new Bitmap(openFileDialog1.FileName);
+
+            for(int x = 0; x < bitmap.Width; ++x)
+                for(int y = 0; y < bitmap.Height; ++y)
+                {
+                    Color color = bitmap.GetPixel(x, y);
+                    double h, s, v;
+                    ToHSV(color, out h, out s, out v);
+                    Color resultColor = FromHSV(h, s, v);
+                    bitmap.SetPixel(x, y, resultColor);
+                    sourceBitmap.SetPixel(x, y, resultColor);
+
+                }
+
+            
+            pictureBox1.Width = bitmap.Width;
+            pictureBox1.Height = bitmap.Height;
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //bitmap.Save(openFileDialog1.FileName + "-toHSV.jpg");
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            this.S = Convert.ToDouble(numericUpDown2.Value);
+            this.updatePictureBox();
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            this.V = Convert.ToDouble(numericUpDown3.Value);
+            this.updatePictureBox();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            bitmap.Save(saveFileDialog1.FileName);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            this.H = Convert.ToDouble(numericUpDown1.Value);
+            this.updatePictureBox();
+        }
+    }
+}
