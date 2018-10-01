d6f1217c38781f4cb8ee159b52d32e57deba0d44
xittz <a.patseev@gmail.com>
2018-10-01 16:19:25 +0200

    Lab #3 task #1 (пукнты а и б) выполнены
    Пацеев Андрей

---
+++ lab3/task-1/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paletteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.templateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem,
+            this.paletteToolStripMenuItem,
+            this.templateToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(381, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(150, 20);
+            this.loadImageToolStripMenuItem.Text = "Загрузить изображение";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // paletteToolStripMenuItem
+            // 
+            this.paletteToolStripMenuItem.Name = "paletteToolStripMenuItem";
+            this.paletteToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.paletteToolStripMenuItem.Text = "Палитра";
+            this.paletteToolStripMenuItem.Click += new System.EventHandler(this.paletteToolStripMenuItem_Click);
+            // 
+            // templateToolStripMenuItem
+            // 
+            this.templateToolStripMenuItem.Name = "templateToolStripMenuItem";
+            this.templateToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
+            this.templateToolStripMenuItem.Text = "Выбрать шаблон";
+            this.templateToolStripMenuItem.Click += new System.EventHandler(this.templateToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 231);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(381, 112);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(138, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(76, 59);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Заливка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 49);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(120, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Заливка рисунком";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(108, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Заливка цветом";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(381, 207);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(234, 56);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(135, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(256, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Размер кисти:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(381, 343);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Task1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paletteToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem templateToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab3/task-1/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_1
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Graphics g2;
+        private Bitmap image;
+        private Bitmap image2;
+        private Color paletteColor;
+        private Color curPixel;
+        private int xG, yG;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            paletteColor = Color.White;
+        }
+
+        // загрузка картинки
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image Files(*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG)|*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image = new Bitmap(openDialog.FileName);
+                    pictureBox1.Image = image;
+                    g = Graphics.FromImage(pictureBox1.Image);
+                    pictureBox1.Invalidate();
+                    Form1.ActiveForm.Width = image.Width + 70;
+                    Form1.ActiveForm.Height = image.Height + menuStrip1.Height + panel1.Height;
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Выбор цвета на палитре
+        private void paletteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                paletteColor = colorDialog1.Color;
+            }
+        }
+
+        // Заливка цветом
+        private void paint(int x, int y, Color curColor, Color newColor)
+        {
+            if (image.GetPixel(x, y) == newColor || image.GetPixel(x, y) != curColor)
+                return;
+            
+            int leftX = x - 1, rightX = x + 1;
+
+            Color pix;
+
+            // Цвет правого пикселя
+            if (rightX != image.Width)
+            {
+                pix = image.GetPixel(rightX, y);
+
+                while (rightX < image.Width && pix == curColor)
+                {
+                    rightX++;
+                    if (rightX < image.Width)
+                        pix = image.GetPixel(rightX, y);
+                }
+            }
+
+
+            // Цвет левого пикселя
+            if (leftX != -1)
+            {
+                pix = image.GetPixel(leftX, y);
+
+                while (leftX >= 0 && pix == curColor)
+                {
+                    leftX--;
+                    if (leftX >= 0)
+                        pix = image.GetPixel(leftX, y);
+                }
+            }
+
+            rightX--;
+            leftX++;
+
+            Pen pen = new Pen(newColor);
+            g.DrawLine(pen, leftX, y, rightX, y);
+
+            for (int i = leftX; i < rightX; ++i)
+            {
+                if (y < image.Height - 1)
+                    paint(i, y + 1, curColor, newColor);
+              
+                if (y > 0)
+                    paint(i, y - 1, curColor, newColor);
+            }
+        }
+
+
+        // Выбрать пиксель, с которого начнется заливка
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                xG = e.Location.X;
+                yG = e.Location.Y;
+                curPixel = (pictureBox1.Image as Bitmap).GetPixel(e.Location.X, e.Location.Y);
+                label2.Text = yG.ToString();
+            }
+        }
+               
+        // Загрузка шаблона
+        private void templateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image Files(*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG)|*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image2 = new Bitmap(openDialog.FileName);
+                    pictureBox2.Image = image2;
+                    g2 = Graphics.FromImage(pictureBox2.Image);
+                    pictureBox2.Invalidate();
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Вызвать заливку цветом или шаблоном
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                if (radioButton1.Checked && curPixel.ToArgb() != paletteColor.ToArgb())
+                {
+                    paint(xG, yG, curPixel, paletteColor);
+                    pictureBox1.Refresh();
+                }
+                else if (radioButton2.Checked)
+                {
+                    if (pictureBox2.Image == null)
+                        MessageBox.Show("Выберите шаблон");
+                    else
+                    {
+                        paintTemplate(xG, yG, 0, curPixel);
+                        pictureBox1.Refresh();
+                    }
+                }
+            }
+        }
+        
+
+        // Заливка шаблоном
+        private void paintTemplate(int xImg, int yImg, int yTemplate, Color oldColor)
+        {
+            //pictureBox1.Refresh();
+
+            int xLeft = xImg - 1, xRight = xImg + 1;
+
+            if (image.GetPixel(xImg, yImg).ToArgb() != oldColor.ToArgb())
+                return;
+            
+            Color pix;
+
+            // Цвет левого пикселя
+            if (xLeft != -1)
+            {
+                pix = image.GetPixel(xLeft, yImg);
+
+                while (xLeft >= 0 && pix == oldColor)
+                {
+                    xLeft--;
+                    if (xLeft >= 0)
+                        pix = image.GetPixel(xLeft, yImg);
+                }
+            }
+
+            pix = image.GetPixel(xRight, yImg);
+
+            // Цвет правого пикселя
+            if (xRight != image.Width)
+            {
+                pix = image.GetPixel(xRight, yImg);
+
+                while (xRight < image.Width && pix == oldColor)
+                {
+                    xRight++;
+                    if (xRight < image.Width)
+                        pix = image.GetPixel(xRight, yImg);
+                }
+            }
+            
+
+            // --- рисуем линию из пикселей шаблона ---
+            int newX;
+            newX = image2.Width - (xG - xLeft) % image2.Width;
+
+            if (yTemplate == -1)
+                yTemplate = image2.Height - 1;
+            ++xLeft;
+            --xRight;
+
+            for (int i = xLeft; i < xRight; ++i)
+            {
+                image.SetPixel(i, yImg, image2.GetPixel((image2.Width / 2 + newX++) % image2.Width, /*(image2.Height / 2 + yTemplate) % image2.Height*/ yTemplate % image2.Height));
+            }
+
+            // ---
+
+            for (int i = xLeft; i < xRight; ++i)
+            {
+                if (yImg < pictureBox1.Image.Height - 1)
+                    paintTemplate(i, yImg + 1, yTemplate + 1, oldColor);
+                if (yImg > 0)
+                    paintTemplate(i, yImg - 1, yTemplate - 1, oldColor);
+            }
+
+        }
+        
+        // Выбрать пиксель, с которого начнется рисование карандашом
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            xG = e.Location.X;
+            yG = e.Location.Y;
+        }
+
+        // Процесс рисования карандашом
+        private void pictureBox1_DrawLine(MouseEventArgs e)
+        {
+            g.DrawLine(new Pen(paletteColor, trackBar1.Value), new Point(xG, yG), e.Location);
+            //g.DrawEllipse(new Pen(paletteColor, 10), xG, yG, 1, 1);
+            xG = e.Location.X;
+            yG = e.Location.Y;
+            pictureBox1.Refresh();
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null && e.Button == MouseButtons.Left)
+                pictureBox1_DrawLine(e);
+        }
+        
+
+
+
+
+    }
+}
