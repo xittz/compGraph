80059f1ab47e5cdf3d4266c7e18f46d6a4f33930
Oleg Smirnov <itech7@ya.ru>
2018-10-02 12:08:17 +0300

    Lab 3 task 2

---
+++ lab3/task-2/graphics32/graphics32/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace graphics32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(95, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(693, 425);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F);
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "No selected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lab3/task-2/graphics32/graphics32/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphics32
+{
+    public partial class Form1 : Form
+    {
+
+        public Bitmap bitmap = null;
+        public Bitmap newBitmap = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+            bitmap = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.ShowDialog();
+            bitmap = new Bitmap("contur1.png");
+            for(int x = 1; x < bitmap.Width; ++x)
+                for (int y = 1; y < bitmap.Height; ++y)
+                {
+                    Color c = bitmap.GetPixel(x, y);
+                    bitmap.SetPixel(x, y, (c.R < 100) ? Color.Black : Color.White);
+                }
+            pictureBox1.Image = bitmap;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (bitmap == null) return;
+            MouseEventArgs mouseEvent = (MouseEventArgs) e;
+
+            label1.Text = "(" + mouseEvent.X + "; " + mouseEvent.Y + ")";
+
+            startPoint = new Point(mouseEvent.X, mouseEvent.Y);
+
+            if(bitmap.GetPixel(startPoint.X, startPoint.Y) == bgColor) return;
+
+            scan(mouseEvent.X, mouseEvent.Y);
+
+            pictureBox1.Image = new Bitmap(newBitmap, new Size(bitmap.Width, bitmap.Height));
+        }
+
+        List<Point> conturList;
+
+        Point startPoint;
+
+        Color bgColor = Color.White;
+
+        Color conturColor;
+
+        Point downDirection = new Point(0, 1);
+        Point downLeftDirection = new Point(-1, 1);
+        Point leftDirection = new Point(-1, 0);
+        Point leftUpDirection = new Point(-1, -1);
+        Point upDirection = new Point(0, -1);
+        Point upRightDirection = new Point(1, -1);
+        Point rightDirection = new Point(1, 0);
+        Point downRightDirection = new Point(1, 1);
+
+        private Point rotate(Point point)
+        {
+            if (point == downDirection)
+                return downLeftDirection;
+            else if (point == downLeftDirection)
+                return leftDirection;
+            else if (point == leftDirection)
+                return leftUpDirection;
+            else if (point == leftUpDirection)
+                return upDirection;
+            else if (point == upDirection)
+                return upRightDirection;
+            else if (point == upRightDirection)
+                return rightDirection;
+            else if (point == rightDirection)
+                return downRightDirection;
+            else
+                return downDirection;
+        }
+
+        private Point pointByDirection(Point point, Point direction )
+        {
+            return new Point(point.X + direction.X, point.Y + direction.Y);
+        }
+
+        private bool isContur(Point point, Point direction)
+        {
+            if (bitmap.GetPixel(point.X, point.Y) != conturColor) return false;
+            
+            Point n = rotate(direction);
+            Point n2 = rotate(n);
+
+            int res = 0;
+
+            if (bitmap.GetPixel(point.X + direction.X, point.Y + direction.Y) != conturColor) ++res;
+            if (bitmap.GetPixel(point.X + n.X, point.Y + n.Y) != conturColor) ++res;
+            if (bitmap.GetPixel(point.X + n2.X, point.Y + n2.Y) != conturColor) ++res;
+            return res > 0;
+        }
+
+        private void scan(int clickX, int clickY)
+        {
+            bgColor = bitmap.GetPixel(1, 1);
+            newBitmap = new Bitmap(bitmap);
+            Point direction = downDirection;
+            Point nextDirection = rotate(direction);
+
+            Point currentPoint = startPoint;
+
+            conturColor = bitmap.GetPixel(clickX, clickY);
+
+            int counter = 0;
+            int pixCounter = 0;
+
+            Point p = currentPoint;
+            Point prevP = p;
+
+            while(bitmap.GetPixel(p.X + nextDirection.X, p.Y + nextDirection.Y) == conturColor)
+            {
+                prevP = p;
+                p = pointByDirection(p, nextDirection);
+            }
+
+            startPoint = p;
+            currentPoint = startPoint;
+
+            //Console.WriteLine("" + currentPoint.X + "; " + currentPoint.Y);
+
+            while (true)
+            {
+                //if (counter > 40000) break;
+
+                newBitmap.SetPixel(currentPoint.X, currentPoint.Y, Color.YellowGreen);
+                newBitmap.SetPixel(currentPoint.X - 1, currentPoint.Y, Color.YellowGreen);
+                newBitmap.SetPixel(currentPoint.X + 1, currentPoint.Y, Color.YellowGreen);
+                newBitmap.SetPixel(currentPoint.X, currentPoint.Y + 1, Color.YellowGreen);
+                newBitmap.SetPixel(currentPoint.X, currentPoint.Y - 1, Color.YellowGreen);
+
+                Point checkPoint = pointByDirection(currentPoint, direction);
+                if (checkPoint == startPoint) break;
+                //Console.WriteLine("" + checkPoint.X + "; " + checkPoint.Y + ": " + bitmap.GetPixel(checkPoint.X, checkPoint.Y).ToString());
+                if (!isContur(checkPoint, nextDirection))
+                {
+                    //Console.WriteLine("S!" + !isContur(checkPoint, nextDirection) + "   " + (bitmap.GetPixel(checkPoint.X, checkPoint.Y) == bgColor));
+                    direction = rotate(direction);
+                    nextDirection = rotate(direction);
+                    //Console.WriteLine("Change direction " + direction.X + "  " + direction.Y);
+                } else
+                {
+                    currentPoint = checkPoint;
+                    ++pixCounter;
+                    
+                }
+                ++counter;
+            }
+
+            Console.WriteLine("Changed " + pixCounter + " pixels");
+            
+
+
+
+        }
+    }
+}
