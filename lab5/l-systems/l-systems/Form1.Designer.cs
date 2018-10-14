0e31f1a0664bc0f80039fda21d122fc36e48e360
xittz <a.patseev@gmail.com>
2018-10-14 16:01:15 +0200

    Lab #5 L-Systems are done
    Patseev A

---
+++ lab5/l-systems/l-systems/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace l_systems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(750, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(774, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 442);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 444);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 487);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ lab5/l-systems/l-systems/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace l_systems
+{
+    public partial class Form1 : Form
+    {
+
+        string a, start;
+        double angle;
+        Dictionary<char, string> rules;
+        List<Tuple<PointF, PointF>> states;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rules = new Dictionary<char, string>();
+            states = new List<Tuple<PointF, PointF>>();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var openDialog = new OpenFileDialog();
+            openDialog.ShowDialog();
+            var file = openDialog.FileName;
+
+            rules = new Dictionary<char, string>();
+            var flines = File.ReadAllLines(file).Where(a => !string.IsNullOrWhiteSpace(a)).ToArray();
+            var args = flines[0].Split(' ');
+
+            a = args[0];
+            angle = (float.Parse(args[1])) * Math.PI / 180;
+            start = args[2];
+
+            foreach (var rl in flines)
+            {
+                if (rl.Contains(">"))
+                {
+                    var rule = rl.Split('>');
+                    rules[char.Parse(rule[0])] = rule[1];
+                }
+            }
+        }
+
+        int it;
+        void draw()
+        {
+            g.Clear(Color.White);
+            string prev, lines = a;
+            float x1 = 0, y1 = 0;
+            float x2 = 0, y2 = 0;
+            for (int i = 0; i < it; i++)
+            {
+                prev = lines;
+                lines = "";
+                foreach (var p in prev)
+                    lines += rules.ContainsKey(p) ? rules[p] : p.ToString();
+            }
+
+            var res = new List<Tuple<PointF, PointF>>();
+
+            if (start == "L")
+            {
+                x1 = pictureBox1.Width;
+                y1 = pictureBox1.Height / 2;
+                x2 = -pictureBox1.Width / 2;
+            }
+            else if (start == "U")
+            {
+                x1 = pictureBox1.Width / 2;
+                y1 = pictureBox1.Height / 2;
+                y2 = -pictureBox1.Height / 2;
+            }
+            else if (start == "R")
+            {
+                y1 = pictureBox1.Height / 2;
+                x2 = pictureBox1.Width / 2;
+            }
+            else if (start == "D")
+            {
+                x1 = pictureBox1.Width / 2;
+                y2 = pictureBox1.Height / 2;
+            }
+
+            var xs = new List<float>();
+            var ys = new List<float>();
+
+            xs.Add(x1);
+            ys.Add(y1);
+
+            foreach (var l in lines)
+                if (l == '[')
+                    states.Add(Tuple.Create(new PointF(x1, y1), new PointF(x2, y2)));
+                else if (l == ']')
+                {
+                    var coords = states[states.Count - 1];
+                    states.RemoveAt(states.Count - 1);
+                    x1 = coords.Item1.X;
+                    y1 = coords.Item1.Y;
+
+                    x2 = coords.Item2.X;
+                    y2 = coords.Item2.Y;
+                }
+                else if (l == '-')
+                {
+                    var temp = x2;
+                    x2 = (float)(temp * Math.Cos(angle) + y2 * Math.Sin(angle));
+                    y2 = (float)(-temp * Math.Sin(angle) + y2 * Math.Cos(angle));
+                }
+                else if (l == '+')
+                {
+                    var temp = x2;
+                    x2 = (float)(temp * Math.Cos(angle) - y2 * Math.Sin(angle));
+                    y2 = (float)(temp * Math.Sin(angle) + y2 * Math.Cos(angle));
+                }
+                else
+                {
+                    res.Add(Tuple.Create(new PointF(x1, y1), new PointF(x1 + x2, y1 + y2)));
+                    x1 += x2;
+                    y1 += y2;
+                    xs.Add(x1);
+                    ys.Add(y1);
+                }
+
+            var scale = Math.Max(xs.Max() - xs.Min(), ys.Max() - ys.Min());
+            foreach (var r in res)
+                g.DrawLine(Pens.Black, (xs.Max() - r.Item1.X) / scale * pictureBox1.Width, (ys.Max() - r.Item1.Y) / scale * pictureBox1.Height,
+                                      (xs.Max() - r.Item2.X) / scale * pictureBox1.Width, (ys.Max() - r.Item2.Y) / scale * pictureBox1.Height);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            it = int.Parse(textBox1.Text);
+            draw();
+            pictureBox1.Invalidate();
+        }
+    }
+}
