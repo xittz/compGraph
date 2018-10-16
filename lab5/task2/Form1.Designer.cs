7214b243c09d97307a1772339dbf15a988f9f6c1
Oleg Smirnov <itech7@ya.ru>
2018-10-16 11:04:54 +0300

    Lab 2 task 2

---
+++ lab5/task2/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Diamond_square
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(567, 437);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(857, 437);
+            this.panel1.TabIndex = 1;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(857, 437);
+            this.splitContainer2.SplitterDistance = 285;
+            this.splitContainer2.SplitterWidth = 5;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.treeView1);
+            this.splitContainer1.Size = new System.Drawing.Size(285, 437);
+            this.splitContainer1.SplitterDistance = 66;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw curve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(4);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(285, 366);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.treeView1_KeyDown);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 340);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(857, 97);
+            this.panel2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(637, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Глубина рекурсии:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(641, 42);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "20";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0,5";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Шероховатость (R) :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 437);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "midpoint displacement";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab5/task2/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Diamond_square.Primitives;
+using System.Threading;
+
+namespace Diamond_square
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+
+        private Point2D startPoint = null;
+        private Point2D endPoint = null;
+
+        private List<Point2D> pointsForDrawing = new List<Point2D>();
+
+        // Список точек для midpoint displacement
+        LinkedList<Point2D> pointsList;
+
+        Random rand = new Random();
+
+        int millisecStep = 50;
+
+        bool isStopped = false;
+
+        private Primitive SelectedPrimitive
+        {
+            get
+            {
+                if (null == treeView1.SelectedNode) return null;
+                var p = (Primitive)treeView1.SelectedNode.Tag;
+                return p;
+            }
+            set
+            {
+                Redraw();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(2048, 2048);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            graphics.Clear(Color.White);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs mouseEvent = (MouseEventArgs)e;
+            Point2D clickedPoint = Point2D.FromPoint(mouseEvent.Location);
+
+            if (startPoint == null)
+            {
+                startPoint = clickedPoint;
+                pointsForDrawing.Add(startPoint);
+            }
+            else if (endPoint == null)
+            {
+                endPoint = clickedPoint;
+                pointsForDrawing.Add(endPoint);
+            }
+            else
+            {
+                pointsForDrawing.Clear();
+                startPoint = null;
+                endPoint = null;
+            }
+
+            Redraw();
+
+            //TreeNode node = treeView1.Nodes.Add("Point (" + clickedPoint.X + ", " + clickedPoint.Y + ")");
+            //node.Tag = clickedPoint;
+
+            //pointsForDrawing.Add(clickedPoint);
+            //Redraw();
+        }
+
+        private void Redraw()
+        {
+            graphics.Clear(Color.White);
+            pointsForDrawing.ForEach((p)
+                => p.Draw(graphics, p == SelectedPrimitive));
+            pictureBox1.Invalidate();
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            SelectedPrimitive = (Primitive)e.Node.Tag;
+            Redraw();
+        }
+
+        private void treeView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keys.Delete != e.KeyCode && Keys.Back != e.KeyCode) return;
+            if (null == SelectedPrimitive) return;
+            if (SelectedPrimitive is Point2D) pointsForDrawing.Remove((Point2D)SelectedPrimitive);
+
+            treeView1.SelectedNode.Remove();
+            if (null != treeView1.SelectedNode)
+                SelectedPrimitive = (Primitive)treeView1.SelectedNode.Tag;
+            else
+                SelectedPrimitive = null;
+            Redraw();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var startPoint = (Point2D)SelectedPrimitive;
+
+            float segmentLength;
+
+            int maxRecDepth;
+
+            double R;
+
+            isStopped = false;
+
+            if (startPoint == null)
+            {
+                MessageBox.Show("Выберите начальную точку");
+                return;
+            }
+
+            if (endPoint == null)
+            {
+                MessageBox.Show("Выберите конечную точку");
+                return;
+            }
+
+            if (!double.TryParse(textBox1.Text, out R))
+            {
+                MessageBox.Show("Неправильное значение шероховатости");
+                return;
+            }
+
+            if (!int.TryParse(textBox3.Text, out maxRecDepth))
+            {
+                MessageBox.Show("Неправильная максимальная глубина рекурсии");
+                return;
+            }
+
+            pointsList = new LinkedList<Point2D>();
+
+
+            //var endPoint = new Point2D(startPoint.X + segmentLength, startPoint.Y);
+            //endPoint.Draw(graphics);
+            //pictureBox1.Refresh();
+
+            //Thread.Sleep(millisecStep);
+
+            pointsList.AddFirst(startPoint);
+            pointsList.AddLast(endPoint);
+
+            graphics.Clear(Color.White);
+
+            (new Edge(startPoint, endPoint)).Draw(graphics);
+
+            pictureBox1.Refresh();
+
+            Thread.Sleep(millisecStep);
+
+            var hl = pointsList.First;
+            var hr = hl.Next;
+
+            Thread t = new Thread(new ThreadStart(() => midpoint(hl, hr, R, maxRecDepth)  ));
+            t.Start();
+
+            DrawCurrentIteration();
+        }
+
+        private void midpoint(LinkedListNode<Point2D> leftPointNode, LinkedListNode<Point2D> rightPointNode, 
+            double R, int currentIteration)
+        {
+            if (currentIteration == 0)
+                return;
+
+            var len = Math.Sqrt(
+                (rightPointNode.Value.X - leftPointNode.Value.X) * (rightPointNode.Value.X - leftPointNode.Value.X)
+                + (rightPointNode.Value.Y - leftPointNode.Value.Y) * (rightPointNode.Value.Y - leftPointNode.Value.Y)
+                );
+
+            var H = (leftPointNode.Value.Y + rightPointNode.Value.Y) / 2;
+
+            var randPart = rand.Next((int)(-R * len), (int)(+R * len));
+
+            H = (H + randPart) < 0 ? H : (H + randPart);
+
+            var newPoint = new Point2D((rightPointNode.Value.X - leftPointNode.Value.X) / 2 + leftPointNode.Value.X, H);
+
+            pointsList.AddAfter(leftPointNode, newPoint);
+
+            if (Math.Abs(newPoint.X - leftPointNode.Value.X) < 1.0) return;
+
+            this.Invoke( (MethodInvoker)delegate
+            {
+                if (isStopped) return;
+                DrawCurrentIteration();
+            });
+
+            //Console.WriteLine("Iteration " + currentIteration + "  size:" + pointsList.Count + " length: " + len + "; " + newPoint.X + "; " + newPoint.Y + "rand: " + randPart);
+
+            midpoint(leftPointNode, leftPointNode.Next, R, currentIteration - 1);
+            midpoint(rightPointNode.Previous, rightPointNode, R, currentIteration - 1);
+        }
+
+        private HashSet<int> drawedItarations = new HashSet<int>();
+
+        private void DrawCurrentIteration()
+        {
+            graphics.Clear(Color.White);
+            var p = pointsList.First;
+            while (p.Next != null)
+            {
+                var edge = new Edge(p.Value, p.Next.Value);
+                edge.Draw(graphics);
+                p = p.Next;
+            }
+            pictureBox1.Refresh();
+            Thread.Sleep(millisecStep);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isStopped = true;
+        }
+    }
+}
